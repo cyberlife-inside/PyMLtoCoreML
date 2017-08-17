226c62d7d8ca39653158c24e31a213040abe1053
julienlange <langejulien@yahoo.fr>
2017-08-18 00:19:19 +0200

    first version (for keras conversion)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PyMLtoCoreML',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Julien LANGE',
+    author_email='julien.lange95@gmail.com',
+    description=''
+)
