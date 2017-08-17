ecfaa62840469c13d87dbf6b60b2b444ae1749ca
julienlange <julien.lange95@gmail.com>
2017-08-18 00:08:53 +0200

    first version (for keras conversion)

---
+++ Py2CoreML.py
@@ -0,0 +1,16 @@
+import argparse
+
+import coremltools
+from keras.models import load_model
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--source', help='file to convert to Apple CoreML model')
+parser.add_argument('--name', help='name to give to the coreML model file')
+args = parser.parse_args()
+
+coremlName = args.name
+filename = args.source
+print (args.source, args.name)
+model = load_model(filepath=args.source)
+coreml_model = coremltools.converters.keras.convert(model)
+coreml_model.save(coremlName + '.mlmodel')
